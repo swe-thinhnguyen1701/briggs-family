7884623fa23baeba5affa4bdd0361262873c170e
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-30 13:02:55 -0500

    styling logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+    const [isClose, setIsClose] = useState(false);
+
     return (
         <nav className="nav">
             <div className="nav-container">
                 <div className="nav-left">
-                    <Link to="/" className="link">
+                    <Link to="/" className="link logo-container">
                         <img src="./src/assets/birgg-logo.webp" alt="Briggs family logo" className="logo"/>
                     </Link>
                 </div>
                 <div className="navright">
-                    <div className="menu-toggle">
+                    <div className={isClose ? "menu-toggle close" : "menu-toggle"} onClick={() => setIsClose(!isClose)}>
                         <span className="bar"></span>
                     </div>
                     <ul className="list menu">
+                        <li className="menu-item">
+                            <Link to="/" className="link">Home</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/" className="link">Home</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/" className="link">Home</Link>
+                        </li>
                         <li className="menu-item">
                             <Link to="/" className="link">Home</Link>
                         </li>
