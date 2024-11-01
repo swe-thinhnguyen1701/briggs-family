1a7622d90df8520e695aed366c578f650577a80e
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-01 14:46:32 -0500

    build policy page

---
+++ src/pages/PolicyPage.jsx
@@ -0,0 +1,116 @@
+/* eslint-disable react/no-unescaped-entities */
+function PolicyPage() {
+    return (
+        <article className="policy-page">
+            <section>
+                <h1>Private Policy</h1>
+                <p>Briggs Family Home Development is committed to protecting online user privacy. In order to ensure your privacy, information Briggs Family Home Development that is collected when you use our website at https://briggsfamilyhd.com ("Site") and respond to Facebook Lead Ads is used only in the manner and for the purposes described in this Briggs Family Home Development Privacy Policy ("Privacy Policy").</p>
+            </section>
+            <section>
+                <h4>User Consent to Privacy Policy</h4>
+                <p>Any person (a) accessing, browsing, or otherwise using the Site, either manually or via an automated device or program or (b) responding to a Facebook Leads Ad, shall be considered a "User". All Users are bound by the terms of this Privacy Policy. Users consent to the collection, use, and disclosure of personally identifiable User information ("Information") pursuant to the terms of this Privacy Policy.</p>
+            </section>
+            <section>
+                <h4>Information Collection and Deletion</h4>
+                <p>
+                    Most of our services do not require any form of registration. However, some services may require you to provide Briggs Family Home Development with Personal Data. In these situations, if you choose to withhold any Personal Data requested by us, it may not be possible for you to gain access to certain parts of the site and/or for us to respond to your query.
+                </p>
+                <p>
+                    Briggs Family Home Development collects and processes information that your provide to us when you use the Site or respond to Facebook Lead Ads, including when you create an account on the Site; interact with Briggs Family Home Development, including contacting us via e-mail, chat functionality, customer review or customer service communications; and purchase products or services through the Site, including the billing and shipping addresses, relevant e-mail and phone numbers, credit card number and expiration date.
+                </p>
+                <p>
+                    You have the right to request access to information we have about you. We will make sure to provide you with a copy of the data we process about you. In order to comply with your request, we may ask you to verify your identity. We will fulfill your request by sending your copy electronically, unless the request specifically specifies a different method.
+                </p>
+                <p>
+                    If you believe the information we have about you is incorrect, you are encouraged to contact us, so we can update it and keep your data accurate. Any data that is no longer needed for the purposes specified in the Use of Information will be deleted. If at any point you wish for Briggs Family Home Development to delete information about you, you can contact us.
+                </p>
+            </section>
+            <section>
+                <h4>Cookies</h4>
+                <p>Through the use of "cookies", Briggs Family Home Development may collect and store anonymous information relating to Users' browsing patterns, including, the User's browser version, site referral information, IP address, operating system, and other technical Site use information.</p>
+
+                <p>The Site uses a feature of Internet Web browsers called a Cookie, which is a file that a User's Web browser places on a User's computer's hard drive, to assign an identification code to the computer and to collect anonymous browsing information. On this Site, cookies are used in conjunction with session variables to track a User's shopping cart and expire as soon as an order is completed or if a shopping cart sits idle for a lengthy period without being checked-out.</p>
+
+                <p>Briggs Family Home Development does not use Cookies to retrieve Information about Users from a User's computer unless the User knowingly and willingly provided such Information.</p>
+
+                <p>You may set your Internet Web browser to notify you when you receive a cookie or to prevent cookies from being sent. If you prevent a cookie from being sent, you may limit the functionality Briggs Family Home Development can provide you when you visit the Site, especially when purchasing an item on the Site.</p>
+            </section>
+            <section>
+                <h4>Use of Information</h4>
+
+                <p>Briggs Family Home Development uses the Information collected from its Users to:</p>
+                <ul>
+                    <li>
+                        <p>Respond to Users' questions, comments, or requests</p>
+                    </li>
+                    <li>
+                        <p>
+                            Administer a User's contest entry or entry in other promotional feature
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Fulfill a User's purchase request and notify Users of their order status
+                        </p>
+                    </li>
+                    <li>
+                        <p>Provide Users with important functionality changes to the site, new Briggs Family Home Development services and special offers we think you will find valuable</p>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h4>Disclosure of Information to Third Parties</h4>
+                <p>Unless we have your consent or except as required or permitted by law, Briggs Family Home Development will not sell, share, trade or give away Information that it collects or receives regarding its Users.</p>
+
+                <p>Briggs Family Home Development may disclose information about our Users to others if we have a good faith belief that we are required to do so by law or legal process, to respond to claims, or to protect the rights, property or safety of Briggs Family Home Development or others. Briggs Family Home Development may disclose Information to third party in the event a third party acquires Briggs Family Home Development or its assets.</p>
+            </section>
+            <section>
+                <h4>Linked Internet Web Sites</h4>
+                <p>The Site may provide hyperlinks, which are highlighted words or pictures within a hypertext document that, when clicked, take you to another place within the document, to another document altogether, or may take you to a third party website not controlled by Briggs Family Home Development. These hyperlinked third-party websites may collect and disclose Information different this Site. Briggs Family Home Development is not responsible for the collection, use, or disclosure of Information collected through these websites, and Briggs Family Home Development expressly disclaims all liability related to such collection, use, or disclosure.</p>
+            </section>
+            <section>
+                <h4>Children</h4>
+                <p>The Briggs Family Home Development is not directed towards children under 18 years of age, and Briggs Family Home Development does not knowingly collect any information from children under 18 years of age through the Site or ads.</p>
+            </section>
+            <section>
+                <h4>Security</h4>
+                <p>Briggs Family Home Development has security measures to protect against the loss, misuse, and alteration of the Information under our control, including credit card information that is submitted to Briggs Family Home Development. When Users place orders or access their account information, the Site uses secure serve software (SSL), which encrypts all information Users input before it is sent to us to protect from unauthorized access. Briggs Family Home Development makes no representations or warranties with regard to the sufficiency of these security measures. Briggs Family Home Development shall not be responsible for any actual or consequential damages that result from a lapse in compliance with this Privacy Policy.</p>
+            </section>
+            <section>
+                <h4>Sales or Acquisition</h4>
+                <p>In the event that another company acquires Briggs Family Home Development in part or all of its assets, we reserve the right to including Information amount the assets transferred to the acquiring company.</p>
+            </section>
+            <section>
+                <h4>Applicable Law</h4>
+                <p>Information that is submitted to this Site will be collected, processed, stored, disclosed, and disposed of in accordance with applicable U.S. law. If you are a non-U.S. User, you acknowledge and agree that we may collect and use your Information, as discussed above, outside your resident jurisdiction. In addition, such Information may be stored on servers located outside your resident jurisdiction. U.S. law may not provide the degree of protection for information that is available in out countries. By providing us with your Information, you acknowledge that you have read this Privacy Policy, understand it, agrees to its terms, and consent to the transfer of such Information outside your resident jurisdiction. If you do not consent to the terms of this Policy, please do not use this Site. If you have already provided us with Information, please contact us and let us know how you would like us to handle such Information.</p>
+            </section>
+            <section>
+                <h4>Controller</h4>
+                <p>Briggs Family Home Development processes Personal data as both a Processor and as a Controller. You can contact Briggs Family Home Development at any time to:</p>
+                <ul>
+                    <li>
+                        <p>
+                            Request access to information that we have about you
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Correct any information we have about you
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Delete any information we have about you
+                        </p>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h4>Amendments to Privacy Policy</h4>
+                <p>Briggs Family Home Development reserves the right to amend this Privacy Policy periodically.</p>
+            </section>
+        </article>
+    )
+}
+
+export default PolicyPage;
