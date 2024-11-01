02763dffc1f3bb0f8ce68a8d750144b37d492862
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-11-01 00:50:39 -0500

    build hero section and add styling, update nav bar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+
 function HomePage() {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <p>Home Page Content</p>
-        </div>
+        <article>
+            <section className="hero">
+                <div className="container">
+                    <h2>professional property loss management</h2>
+                    <span>a family business that treats you like family</span>
+                    <Link to="/" className="link contact-link">
+                        <span>CONTACT US</span>
+                        <FontAwesomeIcon icon={faChevronRight} className="icon" />
+                    </Link>
+                    <div className="background">
+                        <img src="./src/assets/bg-img.webp" alt="" className="background" />
+                    </div>
+                </div>
+            </section>
+        </article>
     )
 }
 
